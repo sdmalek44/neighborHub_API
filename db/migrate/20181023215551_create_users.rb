62243da5a3c3d5e1e17240bdef8ddced2f4a2a97
Angela Duran <duranangela19@gmail.com>
2018-10-24 12:23:13 -0600

    Change users to have first and last name, as well as username

--- db/migrate/20181023215551_create_users.rb
+++ db/migrate/20181023215551_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.string :username
       t.references :district, foreign_key: true
       t.string :email
       t.string :photo

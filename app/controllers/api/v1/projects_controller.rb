0a500dbc85185ff5a318c871313e8cfb07240636
Stephen Malek <malek.stephen44@gmail.com>
2018-10-25 17:18:07 -0600

    patch project test

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -3,5 +3,29 @@ class Api::V1::ProjectsController < ApiController
   def index
     render json: Project.all
   end
-  
+
+  def update
+    project = Project.find(params[:id])
+    if resources_valid? && project && project.update(project_params)
+      resources.each { |resource| project.resources.find(resource[:id]).update(resource.permit(:name, :status)) }
+      render json: {message: "Successfully Updated #{project.title}!"}
+    else
+      render json: {message: "Invalid Input. Try Again"} , status: 400
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description, :photo)
+  end
+
+  def resources
+    params[:project][:resources]
+  end
+
+  def resources_valid?
+    new_resources = resources.select { |resource| resource['id'] && resource['name'] && resource['status'] }
+    new_resources.length == resources.length
+  end
 end
--- spec/requests/api/v1/projects/project_endpoint_spec.rb
+++ spec/requests/api/v1/projects/project_endpoint_spec.rb
@@ -16,5 +16,52 @@ describe '/api/v1' do
       expect(projects[0]['description']).to be_a(String)
       expect(projects[0]['photo']).to be_a(String)
     end
+    it 'PATCH /api/v1/projects/:id' do
+      project = create(:project)
+      resources = create_list(:resource, 2)
+
+      headers = {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+      }
+
+
+      payload = { project: {
+                    title: "build a statue",
+                    description: "We out here building a statue",
+                    photo: "https://aphotoofadamnstatue.com/statue",
+                    resources: [
+                      {
+                        id: resources[0].id,
+                        name: "500 lbs of concrete",
+                        status: "fulfilled"
+                      },
+                      {
+                        id: resources[1].id,
+                        name: "100 laborers",
+                        status: "fulfilled"
+                      }
+                    ]
+                  }
+                }
+      patch "/api/v1/projects/#{project.id}", params: payload.to_json, headers: headers
+
+      expect(response.status).to eq(200)
+
+      message = JSON.parse(response.body, symbolize_names: true)
+
+      expect(message[:message]).to eq("Successfully Updated #{payload[:project][:title]}!")
+      updated_project  = Project.find(project.id)
+      expect(updated_project.title).to eq(payload[:project][:title])
+      expect(updated_project.description).to eq(payload[:project][:description])
+      expect(updated_project.photo).to eq(payload[:project][:photo])
+
+      updated_resource = updated_project.resources[0]
+
+      expect(updated_project.resources.length).to eq(2)
+      expect(updated_resource.id).to eq(resources[0].id)
+      expect(updated_resource.name).to eq(payload[:project][:resources][0][:name])
+      expect(updated_resource.status).to eq(payload[:project][:resources][0][:status])
+    end
   end
 end

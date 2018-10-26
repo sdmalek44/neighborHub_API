4d6f06e1f738f9bd5f10afa32083a6f0f56caac1
Stephen Malek <malek.stephen44@gmail.com>
2018-10-26 17:54:02 -0600

    post user test wip

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,8 @@ class Api::V1::UsersController < ApiController
     render json: User.all
   end
 
+  def create
+    
+  end
+
 end
--- spec/requests/api/v1/users/user_index_spec.rb
+++ spec/requests/api/v1/users/user_index_spec.rb
@@ -1,23 +1,56 @@
 require 'rails_helper'
 
-describe 'GET /api/v1/users' do
-  it 'can get a list of all users' do
-    create(:district)
-    create_list(:user, 4)
+describe '/api/v1/users' do
 
-    get '/api/v1/users'
+  context 'GET /api/v1/users' do
+    it 'can get a list of all users' do
+      create(:district)
+      create_list(:user, 4)
 
-    expect(response).to be_successful
+      get '/api/v1/users'
 
-    users = JSON.parse(response.body)
+      expect(response).to be_successful
 
-    expect(users.count).to eq(4)
-    expect(users[0]['id']).to eq(1)
-    expect(users[0]['first_name']).to be_a(String)
-    expect(users[0]['last_name']).to be_a(String)
-    expect(users[0]['username']).to be_a(String)
-    expect(users[0]['district_id']).to be_a(Integer)
-    expect(users[0]['email']).to be_a(String)
-    expect(users[0]['photo']).to be_a(String)
+      users = JSON.parse(response.body)
+
+      expect(users.count).to eq(4)
+      expect(users[0]['id']).to eq(1)
+      expect(users[0]['first_name']).to be_a(String)
+      expect(users[0]['last_name']).to be_a(String)
+      expect(users[0]['username']).to be_a(String)
+      expect(users[0]['district_id']).to be_a(Integer)
+      expect(users[0]['email']).to be_a(String)
+      expect(users[0]['photo']).to be_a(String)
+    end
+
+  end
+  context 'POST /api/v1/users' do
+    it 'can get a list of all users' do
+      neighborhood = create(:district)
+      headers = {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+      }
+
+      body = {
+                firstName: 'steve',
+                lastName: 'malek',
+                email: 'stevemalek@gmail.com',
+                userName: 'stevie12',
+                neighborhood: district.id,
+                token: 'asdf'
+              }
+
+      post '/api/v1/users', params: body.to_json, headers: headers
+
+      user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user[:id]).to eq(1)
+      expect(user[:first_name]).to eq('steve')
+      expect(user[:last_name]).to eq('malek')
+      expect(user[:username]).to eq('stevie12')
+      expect(user[:email]).to eq('stevemalek@gmail.com')
+      expect(user[:neighborhood]).to eq(neighborhood.district)
+    end
   end
 end

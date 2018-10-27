b61d8c89c751c8414b715dc04a78ff71d4c21b0a
Stephen Malek <malek.stephen44@gmail.com>
2018-10-27 15:00:55 -0600

    create user with password

--- spec/requests/api/v1/users/user_default_login_spec.rb
+++ spec/requests/api/v1/users/user_default_login_spec.rb
@@ -30,5 +30,33 @@ describe '/api/v1/app_users' do
       expect(user[:email]).to eq('stevemalek@gmail.com')
       expect(user[:neighborhood]).to eq(neighborhood.name)
     end
+    it 'can create a new user' do
+      neighborhood = create(:district)
+
+      headers = {
+        "Content-Type": 'application/json',
+        "Accept": 'application/json'
+      }
+
+      body = {
+                first_name: 'steve',
+                last_name: 'malek',
+                email: 'stevemalek@gmail.com',
+                username: 'stevie12',
+                district_id: neighborhood.id,
+                password: 'bluesky'
+              }
+
+      post '/api/v1/app_users', params: body.to_json, headers: headers
+
+      user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user[:id]).to eq(1)
+      expect(user[:first_name]).to eq('steve')
+      expect(user[:last_name]).to eq('malek')
+      expect(user[:username]).to eq('stevie12')
+      expect(user[:email]).to eq('stevemalek@gmail.com')
+      expect(user[:neighborhood]).to eq(neighborhood.name)
+    end
   end
 end

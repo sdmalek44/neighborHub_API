5231ab97a9c464a79a362e9f9b2f606c0ca7349d
Angela Duran <duranangela19@gmail.com>
2018-10-28 12:10:36 -0600

    Updating cors stuff, AGAIN

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://neighborhood-kickstarter.herokuapp.com'
+    origins 'https://neighborhood-kickstarter.herokuapp.com', 'http://localhost:3000'
 
     resource '*',
       headers: :any,

b3d7197251a86dc71fb462c3152c8edd61b6943a
Stephen Malek <malek.stephen44@gmail.com>
2018-10-28 10:59:43 -0600

    change district project query back to how it was

--- app/serializers/district_project_serializer.rb
+++ app/serializers/district_project_serializer.rb
@@ -2,7 +2,9 @@ class DistrictProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :projects
 
   def projects
-    Project.joins(:users, :user_projects).where("users.district_id = #{object.id}").distinct
+    Project.joins(:users, :user_projects)
+      .where("users.district_id = #{object.id} and user_projects.project_role = 1")
+      .uniq
   end
 
 end

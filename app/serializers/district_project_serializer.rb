5a554ba1cd80325ca93c24784ea1de5bab4ece72
Stephen Malek <malek.stephen44@gmail.com>
2018-10-28 10:53:50 -0600

    update district project serializer

--- app/serializers/district_project_serializer.rb
+++ app/serializers/district_project_serializer.rb
@@ -2,7 +2,7 @@ class DistrictProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :projects
 
   def projects
-    Project.joins(:users, :user_projects).where("users.district_id = 24").distinct
+    Project.joins(:users, :user_projects).where("users.district_id = #{object.id}").distinct
   end
 
 end

866d1330c5a3d90a60d8f5517456f4bd27969a8d
Stephen Malek <malek.stephen44@gmail.com>
2018-10-29 10:29:39 -0600

    google oauth project remove

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :email, :neighborhood, :photo, :token
+  attributes :id, :first_name, :last_name, :username, :email, :neighborhood, :photo
 
   def neighborhood
     object.district.name
